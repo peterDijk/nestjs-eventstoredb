e4a70b52850e7bea59daf9329f9241e481bfe2d7
Peter van Dijk <peter.van.dijk@bynder.com>
2023-04-17 13:46:40 +0200

    store multiple updaters, and catch aggregate events not found in stream

--- src/view/view-updater.ts
+++ src/view/view-updater.ts
@@ -16,22 +16,24 @@ export class ViewUpdater {
 
   async run<T extends IEvent>(event: T): Promise<void> {
     this.logger.debug(`running ViewUpdater for ${event.constructor.name}`);
-    const updater = ViewUpdaters.get(event.constructor.name);
-    if (updater) {
-      if (!this.instances.has(updater)) {
-        try {
-          const moduleUpdater = this.moduleRef.get(updater, {
-            strict: false,
-          });
-          this.instances.set(updater, moduleUpdater);
-        } catch (err) {
-          this.logger.debug(err);
+    const updaters = ViewUpdaters.get(event.constructor.name);
+    if (updaters) {
+      updaters.forEach(async updater => {
+        if (!this.instances.has(updater)) {
+          try {
+            const moduleUpdater = this.moduleRef.get(updater, {
+              strict: false,
+            });
+            this.instances.set(updater, moduleUpdater);
+          } catch (err) {
+            this.logger.debug(err);
+          }
         }
-      }
-      this.logger.debug(
-        `found updater for event ${event.constructor.name} - calling handle method`,
-      );
-      await this.instances.get(updater).handle(event);
+        this.logger.debug(
+          `found updater for event ${event.constructor.name} - calling handle method`,
+        );
+        await this.instances.get(updater).handle(event);
+      });
     }
     return;
   }
--- src/view/view-updaters.ts
+++ src/view/view-updaters.ts
@@ -3,15 +3,16 @@ import { IViewUpdater } from './interfaces/view-updater';
 import { Type, Logger } from '@nestjs/common';
 
 export class ViewUpdaters {
-  private static updaters = new Map<string, Type<IViewUpdater<IEvent>>>();
+  private static updaters = new Map<string, Type<IViewUpdater<IEvent>>[]>();
   private static logger = new Logger(ViewUpdaters.name);
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   static add(name: string, handler: Type<IViewUpdater<IEvent>>) {
-    ViewUpdaters.updaters.set(name, handler);
+    const existingUpdater = ViewUpdaters.updaters.get(name);
+    ViewUpdaters.updaters.set(name, [...existingUpdater, handler]);
   }
 
-  static get(name: string): Type<IViewUpdater<IEvent>> {
+  static get(name: string): Type<IViewUpdater<IEvent>>[] {
     return ViewUpdaters.updaters.get(name);
   }
 }
